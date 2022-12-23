25a8dab0860ed565f32d32a2ecdf68b35cd39189
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-23 18:11:15 +0530

    What I expect to make by the means of this repository.

---
+++ roughtIdea.sh
@@ -0,0 +1,18 @@
+<<COMMENTS
+Running this file.
+Should Install java, python, c++, and other languages with it.
+It should set the swappiness to appropriately, by asking the user.
+It should update the system.
+It should make a swap memory file by taking input from the user.
+Should update the mirrors.
+Should install the vscode, pycharm, by asking the user whether he wants to
+    install it or not.
+It should take permission of the user at every stage.
+Or it should take the persmission of user before only.
+So it doesn't have to wait, for it. At start only it should take the
+Permission to do so.
+What git repository to download, which are important.
+Chsh also.
+Replace certain files if needed.
+and Stuffs.
+COMMENTS
